99e8b60f09ab1bcd47db5a024352b653a55be803
Young <ylee1359256@gmail.com>
2023-05-02 01:17:19 -0400

    created password generator

--- main.py
+++ main.py
@@ -1,8 +1,28 @@
 from tkinter import *
 from tkinter import messagebox
-
+from random import choice, randint, shuffle
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
+def generate_password():
+
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+
+
+    pass_letter = [choice(letters) for char in range(randint(8, 10))]
+
+    pass_symbol = [choice(symbols) for char in range(randint(2, 4))]
+
+    pass_number = [choice(numbers) for char in range(randint(2, 4))]
+
+    password_list = pass_letter + pass_symbol + pass_number
+    shuffle(password_list)
+
+    password = "".join(password_list)
+
+    password_entry.insert(0,f"{password}")
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def save():
@@ -52,7 +72,7 @@ password_label.grid(row=3, column=0)
 password_entry = Entry(width=35)
 password_entry.grid(row=3, column=1, columnspan=2)
 
-generate_button = Button(text="Generate Password")
+generate_button = Button(text="Generate Password", command=generate_password)
 generate_button.grid(row=3, column=2, columnspan=2)
 
 add_button = Button(text="Add", width=36, command=save)

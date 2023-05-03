6873056899ccaf2172b68438aaae3deab1994aa8
Young <ylee1359256@gmail.com>
2023-05-03 00:01:24 -0400

    changed saved password from .txt to json

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from tkinter import *
 from tkinter import messagebox
 from random import choice, randint, shuffle
+import json
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 def generate_password():
@@ -8,8 +9,6 @@ def generate_password():
     letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
     numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
     symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
-
-
 
     pass_letter = [choice(letters) for char in range(randint(8, 10))]
 
@@ -22,24 +21,43 @@ def generate_password():
 
     password = "".join(password_list)
 
-    password_entry.insert(0,f"{password}")
+    password_entry.insert(0, f"{password}")
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def save():
+    data = {
+        web_entry.get(): {
+            "email": email_entry.get(),
+            "password": password_entry.get(),
+         }
+    }
 
     if len(web_entry.get()) == 0 or len(password_entry.get()) == 0:
         messagebox.showwarning(title="Oops", message="Please don't leave any fields empty")
     else:
-        yes_no = messagebox.askokcancel(title=web_entry.get(), message=f"These are the details entered: \n "
-                                                              f"Email: {email_entry.get()} \n "
-                                                              f"Password: {password_entry.get()} \n"
-                                                              f"Is it ok to save?")
-    if yes_no:
-        with open("data.txt", "a") as file:
-            file.write(f"{web_entry.get()}|{email_entry.get()}|{password_entry.get()}\n")
-        web_entry.delete(0, END)
-        password_entry.delete(0, END)
-        web_entry.focus()
+        try:
+
+            with open("data.json", 'r') as file:
+                # read old data
+                new_data = json.load(file)
+
+        except FileNotFoundError:
+
+            with open("data.json", 'w') as file:
+                json.dump(data, file, indent=4)
+
+        else:
+            # update with new data
+            new_data.update(data)
+
+            with open("data.json", "w") as file:
+                # saving updated data
+                json.dump(new_data, file, indent=4)
+
+        finally:
+            web_entry.delete(0, END)
+            password_entry.delete(0, END)
+            web_entry.focus()
 # ---------------------------- UI SETUP ------------------------------- #
 # create window
 window = Tk()
